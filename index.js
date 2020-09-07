c9487f575a9135317a926db769ee6a4fc7e8d0d6
BjarkeHou <mail@bjarkehou.dk>
2020-09-07 18:04:14 +0200

    Added roll module

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const Roll = require('./modules/roll.js')
+
+const prefix = "!"
+
+// Discord client setup
+const client = new Discord.Client();
+client.login(config.BOT_TOKEN);
+
+client.on('message', msg => {
+	// Dont react to bot msgs and normal msgs
+	if (msg.author.bot) return;
+	if (!msg.content.startsWith(prefix)) return;
+	  	
+	const commandBody = msg.content.slice(prefix.length);
+  	const args = commandBody.split(' ');
+  	const command = args.shift().toLowerCase();
+
+  	if(command==='roll') {
+  		msg.reply(Roll.roll(args))
+  	}
+});
---
+++ modules/roll.js
@@ -0,0 +1,20 @@
+// File roll.js
+module.exports = {
+    roll: function(args) {
+        var min = 1, max = 100
+        if(args.length == 2 && parseInt(args[0]) && parseInt(args[1]) && args[0] < args[1]) {
+            min = args[0]
+            max = args[1]
+        }
+
+        min = Math.ceil(min);
+        max = Math.floor(max);
+
+        number = Math.floor(Math.random() * (max - min) + min)
+        return 'Roll: **'+number+'**'
+    }
+};
+
+
+
+        
